d3a4a746a8fb5da90982c515ee0c1b9d564f80c1
davebrooks199909@gmail.com <davebrooks199909@gmail.com>
2018-10-29 20:26:27 +0000

    Commit 4
    After thinking about how parallax could make the game good or bad. I decided that screenwrapping had to implemented. Without it The game would break for NPCs being spawned would be left behind and if childed to the sprited background they would be restricted and there is only so much there can be done with difficulty in a fixed location. So for now ive added a basic screenwrapping script for 2 box colliders that act as triggers. However later i will be controlling the screenwrapping through the base class

---
+++ The Great Defender/Assets/DB_Scripts/ScreenWrap/ScreenWrap_Left.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenWrap_Left : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            other.gameObject.transform.position = new Vector3(69.5f, other.gameObject.transform.position.y, 0);
+        }
+    }
+}
